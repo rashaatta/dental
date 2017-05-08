e6015b3d3656089612b1a683e310fcb178fd14e4
Mohammed Mahdy <mohamed.mahdy@sphinxinfotech.net>
2017-05-08 16:19:39 +0200

    add confirmation msg in staff grid

--- resources/views/staff/index.blade.php
+++ resources/views/staff/index.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.app')
+@include('layouts.error')
 
+<?php
+
+use Illuminate\Support\Facades\URL;
+
+ function buttonDelete($id)
+{
+    $format = '<a href="%s" data-toggle="tooltip" data-delete="%s" title="%s" class="btn btn-default"><i
+            class="fa fa-trash-o"></i></a>';
+    $link = "/staff/delete/$id";
+    $token = csrf_token();
+    $title = "Delete the doctor";
+    return sprintf($format, $link, $token, $title);
+};
+
+
+?>
 @section('content')
     <!--<ul>
         @foreach($staff as $st)
@@ -17,7 +34,8 @@
                 <a href="/staff/add/">
                     <i class="fa fa-plus" aria-hidden="true" title="add"></i>
                     {{--<span class="glyphicon glyphicon-trash"></span>--}}
-                </a></td>
+                </a>
+            </td>
 
             </th>
         </tr>
@@ -32,10 +50,14 @@
                         <i class="fa fa-pencil-square-o" aria-hidden="true" title="edit"></i>
                         {{--<span class="glyphicon glyphicon-edit"></span>--}}
                     </a>
-                    <a href="/staff/delete/{{$st->id}}">
-                        <i class="fa fa-remove" aria-hidden="true" title="delete"></i>
-                        {{--<span class="glyphicon glyphicon-trash"></span>--}}
-                    </a></td>
+                {{--<a href="/staff/delete/{{$st->id}}">--}}
+                {{--<i class="fa fa-remove" aria-hidden="true" title="delete"></i>--}}
+                {{--<span class="glyphicon glyphicon-trash"></span>--}}
+                {{--</a>--}}
+                <!--                    -->
+
+                    <?= buttonDelete($st->id) ?>
+                </td>
 
             </tr>
         @endforeach
@@ -43,4 +65,33 @@
         </tbody>
     </table>
 
+@endsection
+
+
+@section('script')
+
+    <script type="text/javascript">
+
+        // Jquery function which listens for click events on elements which have a data-delete attribute
+        $('[data-delete]').click(function(e){
+            e.preventDefault();
+            // If the user confirm the delete
+            if (confirm('Do you really want to delete the element ?')) {
+                // Get the route URL
+                var url = $(this).prop('href');
+                // Get the token
+                var token = $(this).data('delete');
+                // Create a form element
+                var $form = $('<form/>', {action: url, method: 'get'});
+                // Add the DELETE hidden input method
+                var $inputMethod = $('<input/>', {type: 'hidden', name: '_method', value: 'get'});
+                // Add the token hidden input
+                var $inputToken = $('<input/>', {type: 'hidden', name: '_token', value: token});
+                // Append the inputs to the form, hide the form, append the form to the <body>, SUBMIT !
+                $form.append($inputMethod, $inputToken).hide().appendTo('body').submit();
+//                console.log('done!!!!');
+            }
+        });
+
+    </script>
 @endsection
